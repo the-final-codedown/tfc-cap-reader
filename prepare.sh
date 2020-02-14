d39fc98f7213ca6d968ffe525086a5302e17a0b2
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-14 20:16:00 +0100

    Updated prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build -t tfc/tfc-profile .
+IMAGE=tfc/profile
+VERSION=
+
+docker build -t ${IMAGE} .
+docker tag ${IMAGE} localhost:5000/${IMAGE}${VERSION}
+docker push localhost:5000/${IMAGE}${VERSION}
